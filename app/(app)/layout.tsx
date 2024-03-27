c1669980c067da8a046797a1cb79554ade72f5a5
Ali Bin Naseer <aliisinsane0089@gmail.com>
2024-03-27 12:18:07 +0500

    Bas ho gai, I quit

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -3,24 +3,29 @@
 import { getUser } from "@/lib/features/user/userThunk";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
     const dispatch = useAppDispatch();
     const { authenticated } = useAppSelector((state) => state.user);
+    const [isLoading, setIsLoading] = useState(true);
 
     const router = useRouter();
 
     useEffect(() => {
-        dispatch(getUser());
-        if (authenticated) {
-            router.push("/channels/me");
-            console.log("Authenticated in AppLayout!");
-        }
+        (async () => {
+            setIsLoading(false);
+            await dispatch(getUser());
+        })();
 
-        if (!authenticated) {
-            router.push("/login");
-            console.log("Not Authenticated in AppLayout!");
+        if (!isLoading) {
+            if (authenticated) {
+                router.push("/channels/me");
+                console.log("Authenticated in AppLayout!");
+            } else {
+                router.push("/login");
+                console.log("Not Authenticated in AppLayout!");
+            }
         }
     }, [authenticated]);
 
